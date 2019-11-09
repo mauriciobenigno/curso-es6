32b856d2c624833db18a2004558fd530ac01f668
Mauricio Benigno <mauriciobenigno@hotmail.com>
2019-11-09 20:08:16 -0300

    Valores padrão

--- bundle.js
+++ bundle.js
@@ -1,138 +1,20 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var List =
-/*#__PURE__*/
-function () {
-  function List() {
-    _classCallCheck(this, List);
-
-    this.data = [];
-  }
-
-  _createClass(List, [{
-    key: "add",
-    value: function add(data) {
-      this.data.push(data);
-      console.log(this.data);
-    }
-  }]);
-
-  return List;
-}();
-
-var TodoList =
-/*#__PURE__*/
-function (_List) {
-  _inherits(TodoList, _List);
-
-  function TodoList() {
-    var _this;
-
-    _classCallCheck(this, TodoList);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(TodoList).call(this));
-    _this.usuario = 'Mauricio';
-    return _this;
-  }
-
-  _createClass(TodoList, [{
-    key: "mostraUsuario",
-    value: function mostraUsuario() {
-      console.log(this.usuario);
-    }
-  }]);
-
-  return TodoList;
-}(List);
-
-var MinhaList = new TodoList();
-
-document.getElementById('novotodo').onclick = function () {
-  MinhaList.add('Novo Todo');
-}; //MinhaList.mostraUsuario();
+// Usando o método comum
+function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+} // usando Arrow function
 
 
-function teste(x) {
-  var y = 2;
-
-  if (x > 5) {
-    var _y = 4;
-    console.log(x, _y);
-  }
-} //teste(10);
-//const arr = [1, 3, 4, 5, 8, 10];
+var soma2 = function soma2() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+}; //console.log(soma(1));
+//console.log(soma());
 
 
-var arr = [1, 3, 4, 5, 8, 10]; // ARROW FUNCTIONS
-
-var newArrf = arr.map(function (item) {
-  return item * 2;
-}); //console.log(newArrf);
-
-var newArrf2 = arr.map(function (item) {
-  return item * 2;
-}); //console.log(newArrf2);
-
-var newArrf3 = arr.map(function (item) {
-  return item * 2;
-}); //console.log(newArrf3);
-
-var testeF = function testeF() {
-  return 'teste'; // retorna uma string
-}; //console.log(testeF());
-
-
-var testeF2 = function testeF2() {
-  return [1, 2, 3];
-}; // retorna array
-//console.log(testeF2());
-
-
-var testeF3 = function testeF3() {
-  return 3;
-}; // retorna valor
-//console.log(testeF3());
-
-
-var testeF4 = function testeF4() {
-  return {
-    nome: 'Mauricio'
-  };
-}; // retorna objeto
-//console.log(testeF4());
-// OPERAÇÕES COM ARRAY
-
-
-var newArr = arr.map(function (item) {
-  return item * 2;
-}); //console.log(newArr);
-
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-}); //console.log(sum);
-
-var filter = arr.filter(function (item) {
-  return item % 2 == 0;
-}); //console.log(filter);
-
-var find = arr.find(function (item) {
-  return item === 4;
-}); //console.log(find);
+console.log(soma2(1));
+console.log(soma2());
--- main.js
+++ main.js
@@ -1,113 +1,14 @@
-
-class List{
-    constructor(){
-        this.data = [];
-    }
-
-    add(data){
-        this.data.push(data);
-        console.log(this.data);
-    }
-}
-
-class TodoList extends List{
-    constructor()
-    {
-        super();
-
-        this.usuario = 'Mauricio';
-    }
-
-    mostraUsuario()
-    {
-        console.log(this.usuario);
-    }
-
-}
-
-var MinhaList = new TodoList();
-
-document.getElementById('novotodo').onclick = function(){
-    MinhaList.add('Novo Todo');
-}
-
-//MinhaList.mostraUsuario();
-
-function teste(x)
+// Usando o método comum
+function soma(a=3,b=6)
 {
-    let y = 2;
-
-    if( x > 5)
-    {
-        let y = 4;
-        console.log(x, y);
-    }
+    return a+b;
 }
+// usando Arrow function
+const soma2 = (a = 3, b = 6) => a + b ;
 
-//teste(10);
+//console.log(soma(1));
+//console.log(soma());
 
-//const arr = [1, 3, 4, 5, 8, 10];
-const arr = [1, 3, 4, 5, 8, 10];
-
-// ARROW FUNCTIONS
-
-const newArrf = arr.map((item) => {
-    return item * 2;
-});
-
-//console.log(newArrf);
-
-const newArrf2 = arr.map(item => {
-    return item * 2;
-});
-
-//console.log(newArrf2);
-
-const newArrf3 = arr.map(item => item * 2);
-
-//console.log(newArrf3);
-
-const testeF = () => {
-    return 'teste'; // retorna uma string
-};
-
-//console.log(testeF());
-
-const testeF2 = () => [1,2,3]; // retorna array
-
-//console.log(testeF2());
-
-const testeF3 = () => 3; // retorna valor
-
-//console.log(testeF3());
-
-const testeF4 = () => ({ nome: 'Mauricio'}); // retorna objeto
-
-//console.log(testeF4());
-
-// OPERAÇÕES COM ARRAY
-
-const newArr = arr.map(function(item){
-    return item*2;
-});
-
-//console.log(newArr);
-
-const sum = arr.reduce(function(total, next){
-    return total+ next;
-});
-
-//console.log(sum);
-
-const filter = arr.filter(function(item){
-    return item % 2 == 0;
-});
-
-//console.log(filter);
-
-const find = arr.find(function(item){
-    return item === 4;
-});
-
-//console.log(find);
+console.log(soma2(1));
+console.log(soma2());
 
