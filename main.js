2d8792feab3a4f80ebc7b847b29bab4096a7a2e3
Mauricio Benigno <mauriciobenigno@hotmail.com>
2019-11-09 17:27:09 -0300

    Classes

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-alert('Teste');
 
-class Teste{
-    metodo(){
-        
+class List{
+    constructor(){
+        this.data = [];
+    }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor()
+    {
+        super();
+
+        this.usuario = 'Mauricio';
+    }
+
+    mostraUsuario()
+    {
+        console.log(this.usuario);
+    }
+
+}
+
+var MinhaList = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaList.add('Novo Todo');
+}
+
+MinhaList.mostraUsuario();
