6aa35816ceceb1cf733acad84a040714f93fb96e
Mauricio Benigno <mauriciobenigno@hotmail.com>
2019-11-09 19:46:13 -0300

    Operações em Array

--- main.js
+++ main.js
@@ -31,4 +31,44 @@ document.getElementById('novotodo').onclick = function(){
     MinhaList.add('Novo Todo');
 }
 
-MinhaList.mostraUsuario();
+//MinhaList.mostraUsuario();
+
+function teste(x)
+{
+    let y = 2;
+
+    if( x > 5)
+    {
+        let y = 4;
+        console.log(x, y);
+    }
+}
+
+//teste(10);
+// OPERAÇÕES COM ARRAY
+
+const arr = [1, 3, 4, 5, 8, 10];
+
+const newArr = arr.map(function(item){
+    return item*2;
+});
+
+//console.log(newArr);
+
+const sum = arr.reduce(function(total, next){
+    return total+ next;
+});
+
+//console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 == 0;
+});
+
+//console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 4;
+});
+
+//console.log(find);
