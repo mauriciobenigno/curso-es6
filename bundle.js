14feb789cfb620ac741f6e0334f1b6fdc1a41697
Mauricio Benigno <mauriciobenigno@hotmail.com>
2019-11-09 21:09:23 -0300

    Template literals

--- bundle.js
+++ bundle.js
@@ -1,29 +1,7 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+// Template Literals
+var nome = "Maurício";
+var idade = 24; // é o uso de crase para simplicar a modelagem de strings com variaveis
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-// SPREAD
-
-/** Consiste na passagem de informação de uma estrutura para outra */
-// Exemplo 1  - usando spread para concatenar os arrays
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2); //console.log(arr3);
-// Exemplo 2 - Consiste em copiar todas as informações de um objeto sobrepondo uma delas
-
-var usuario1 = {
-  nome: 'Mauricio',
-  idade: 24,
-  empresa: 'BenignoDev'
-};
-
-var usuario2 = _objectSpread({}, usuario1, {
-  nome: 'Jose'
-}); // copia todo o conteúdo mas troca o nome
-
-
-console.log(usuario2);
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-// SPREAD
-/** Consiste na passagem de informação de uma estrutura para outra */
-// Exemplo 1  - usando spread para concatenar os arrays
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-
-const arr3 = [...arr1, ...arr2];
-//console.log(arr3);
-
-// Exemplo 2 - Consiste em copiar todas as informações de um objeto sobrepondo uma delas
-
-const usuario1 = {
-    nome:'Mauricio',
-    idade:24,
-    empresa:'BenignoDev',
-};
-
-const usuario2 = { ...usuario1, nome:'Jose'}; // copia todo o conteúdo mas troca o nome
-
-console.log(usuario2);
+// Template Literals
+const nome = "Maurício";
+const idade = 24;
+// é o uso de crase para simplicar a modelagem de strings com variaveis
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
