9eb48f5be31a1c258d0a295f87a5f8add029756a
Mauricio Benigno <mauriciobenigno@hotmail.com>
2019-11-11 17:55:08 -0300

    Resolução tópico 3 exercicio ES6+

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 
-const getUserFromGithub = (user) => new Promise((resolve,reject) =>{
-    axios.get(`https://api.github.com/users/${user}`)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(err => {
-        console.log('Usuário não existe');
-    })
- });
 
- const iniciaBusca = async(user) => {
-    console.log(await getUserFromGithub('mauriciobenigno'));    ;    
+class Github {
+    static async getRepositories(repo) 
+    {
+        try {
+            const response  = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data);
+        } catch (error) {
+            console.log('Repositório não existe');
+        }
+    }
 }
 
+const iniciaBusca = async() => {
+    console.log(await Github.getRepositories('mauriciobenigno/BasicoJSRocketSeatMod4')); 
+    console.log(await Github.getRepositories('rocketseat/dslkvmskv')); 
+
+}
+   
 iniciaBusca();
