b25cbf82b542a5157d14d6fbde5ab38d2f8397ea
Mauricio Benigno <mauriciobenigno@hotmail.com>
2019-11-11 17:15:52 -0300

    Utilizando async e await

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import ClasseUsuario from './functions'
-import {idade as IdadeUsuario} from './functions'
+const minhaPromisse = () => new Promise((resolve,reject) =>{
+   setTimeout(() => { resolve('OK') }, 2000); 
+});
 
-ClasseUsuario.info();
-console.log(IdadeUsuario);
+/*
+minhaPromisse()
+    .then(response => {
+        console.log(response);
+    });*/
+
+// Função assincrona, possuí prefixo async
+// Ex 1 normal
+/*async function executaPromisse() {
+    console.log(await minhaPromisse());    
+    console.log(await minhaPromisse());    
+    console.log(await minhaPromisse());    
+}*/
+// Ex 2 - com arrow funcion
+const executaPromisse = async() => {
+    console.log(await minhaPromisse());    
+    console.log(await minhaPromisse());    
+    console.log(await minhaPromisse());    
+}
+
+executaPromisse();
